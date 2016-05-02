b84bec3ad62b6579884ec8e2adf16f58f2addbd6
Haney Family Computer <robert.a.haney@gmail.com>
2016-05-02 19:14:08 -0400

    Added template variables to app.js and deleted helper.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -93,6 +93,8 @@ var model = {
  }
 };
 
+// Display data
+
 var view = {
  bioDisplay: function() {
   	var formatted = controller.formatBio();
@@ -144,6 +146,36 @@ var view = {
     };
  }
 };
+
+// Predefined variable templates for formatting data
+
+var HTMLheaderName = '<h1>%data%</h1>';
+var HTMLheaderRole = '<span>%data%</span>';
+
+var HTMLemail = '<span class="pink-text">email</span><span class="white-text">%data%</span>';
+var HTMLgithub = '<span class="pink-text">github</span><span class="white-text">%data%</span>';
+
+var HTMLskills = '<li><span class="white-text" id="skills-text">%data%</span></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="%url%">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="%link%" id="project-title">%data%</a>';
+var HTMLprojectDescription = '<p><em>%data%</em></p>';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="%url%">%data%';
+var HTMLschoolDegree = ': %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+// Format data for display
 
 var controller = {
   init: function() {
